932da69068fd2976556288e9a7079b5825730e1e
qizhanming <qizhanming@cedex.cn>
2019-08-29 15:33:52 +0800

    update dependence and change style.
    bootstrap 4.3, jquery 3.3.1 and jeklly-toc 1.0.8.

--- assets/app/js/app.js
+++ assets/app/js/app.js
@@ -5,3 +5,40 @@ jQuery(document).ready(function() {
 		jQuery(this).attr('target', '_blank');
 	});
 });
+
+function highlightRightNav(heading)
+{
+  if (document.location.pathname.indexOf("/glossary/") < 0){
+    $("#my_toc a.active").removeClass("active");
+
+    if (heading !== "title") {
+      $("#my_toc a[href='#" + heading + "']").addClass('active');
+    }
+  }
+}
+
+var currentHeading = "";
+jQuery(window).scroll(function(){
+  var headingPositions = new Array();
+  jQuery("h1, h2, h3, h4, h5, h6").each(function(){
+    if (this.id == "") this.id="title";
+    headingPositions[this.id]=this.getBoundingClientRect().top;
+  });
+  headingPositions.sort();
+  // the headings have all been grabbed and sorted in order of their scroll
+  // position (from the top of the page). First one is toppermost.
+  for(var key in headingPositions)
+  {
+    if (headingPositions[key] > 0 && headingPositions[key] < 200)
+    {
+      if (currentHeading != key)
+      {
+        // a new heading has scrolled to within 200px of the top of the page.
+        // highlight the right-nav entry and de-highlight the others.
+        highlightRightNav(key);
+        currentHeading = key;
+      }
+      break;
+    }
+  }
+});
